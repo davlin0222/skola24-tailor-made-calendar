2a3b2a94473692e1e505776c8ca1db2130d5a32e
DavidLindström <davlin@creatorise.com>
2021-05-22 19:50:33 +0200

    Schedule form

---
+++ public/main.js
@@ -0,0 +1,26 @@
+import { DateTime } from './vendors/luxon.js';
+// const date = DateTime.local(2021, 5, 24).weekNumber;
+// console.log('🚀 ~ file: main.js ~ line 5 ~ date', date);
+
+const form = document.querySelector('form');
+
+form.week_number.value = DateTime.now().weekNumber;
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const schedule_id = e.target.schedule_id.value;
+  const week_number = e.target.week_number.value;
+  console.log('form values:', e.target.schedule_id.value, week_number);
+
+  const data = { schedule_id, week_number };
+
+  fetch('/', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    // body: JSON.stringify(new FormData(e.target)),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+});
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
+// HTTP loggers
+app.use(require('morgan')('dev'));
+// app.use(require('pino-http')({ prettyPrint: true }));
+
+// Middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+// Routes
+
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.sendFile(path.join(__dirname, 'public', 'main.html'));
+});
+
+app.post('/', (req, res) => {
+  // console.log('🚀 ~ file: server.js ~ line 14 ~ app.post ~ req', req);
+  console.log('🚀 ~ file: server.js ~ line 13 ~ app.post ~ req.body', req.body);
+  res.send(req.body);
 });
 
 const PORT = 3000;
