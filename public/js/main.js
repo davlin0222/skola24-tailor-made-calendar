e9b0edad7d0fe741b03848974bfa63617eff333d
viunea <viunea@creatorise.com>
2021-08-25 17:59:40 +0200

    Refactor MVP public html and js

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+import { DateTime } from '../vendors/luxon.js';
+import download_blob_as_file from './download_blob_as_file.js';
+
+const custom_calendar__form = document.querySelector('.custom_calendar__form');
+
+custom_calendar__form.week_number.value = DateTime.now().weekNumber;
+
+custom_calendar__form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const data = {
+        schedule_id: e.target.schedule_id.value,
+        week_number: e.target.week_number.value,
+    };
+
+    fetch('/create-calendar', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((res) => res.blob())
+        .then(async (blob) => {
+            const data_string = await blob.text();
+            download_blob_as_file(data_string, 'school.ics');
+        });
+});
